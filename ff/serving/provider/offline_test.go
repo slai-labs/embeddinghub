48b94a842ee9bece1907d7412e1a44a2b806f66f
sdreyer <sterling@featureform.com>
2022-04-20 11:05:55 -0700

    Changed Schema and Interfaces for failing tests

--- ff/serving/provider/offline_test.go
+++ ff/serving/provider/offline_test.go
@@ -64,10 +64,10 @@ func TestOfflineStores(t *testing.T) {
 		"LabelTableNotFound":      testLabelTableNotFound,
 		"FeatureTableNotFound":    testFeatureTableNotFound,
 		"TrainingDefShorthand":    testTrainingSetDefShorthand,
-		"PrimaryTableCreate":      testPrimaryCreateTable,
-		"PrimaryTableWrite":       testPrimaryTableWrite,
 	}
 	testSQLFns := map[string]func(*testing.T, SQLOfflineStore){
+		"PrimaryTableCreate": testPrimaryCreateTable,
+		"PrimaryTableWrite":  testPrimaryTableWrite,
 		"Transformation":     testTransform,
 		"TransformToFeature": testTransformCreateFeature,
 	}
@@ -905,7 +905,7 @@ func testTrainingSetDefShorthand(t *testing.T, store OfflineStore) {
 	}
 }
 
-func testPrimaryCreateTable(t *testing.T, store OfflineStore) {
+func testPrimaryCreateTable(t *testing.T, store SQLOfflineStore) {
 	type TestCreateCase struct {
 		Rec         ResourceID
 		Schema      TableSchema
@@ -989,7 +989,7 @@ func testPrimaryCreateTable(t *testing.T, store OfflineStore) {
 	}
 }
 
-func testPrimaryTableWrite(t *testing.T, store OfflineStore) {
+func testPrimaryTableWrite(t *testing.T, store SQLOfflineStore) {
 	type TestCase struct {
 		Rec         ResourceID
 		Schema      TableSchema
